565669a6220c50963dced00b0cbafaba2fcfaf28
Sagarspoojary <sagar.23cs125@sode-edu.in>
2024-10-24 15:53:35 +0530

    Add files via upload

--- lab8.py
+++ lab8.py
@@ -16,3 +16,13 @@ fig=figure(min_width=800,min_height=200)
 fig.vbar(x=[2,4,6],width=1,bottom=0,top=[10,8,4],color='Cyan')
 output_file('vbar.html')
 show(fig)
+from bokeh.plotting import figure,output_file,show
+p=figure(min_width=800,min_height=200)
+p.patch(x=[1,3,2,4],y=[2,3,5,7],color='green')
+output_file('patch.html')
+show(p)
+from bokeh.plotting import figure,output_file,show
+p=figure(min_width=800,min_height=400)
+p.scatter([1,4,3,2,5],[6,5,2,4,7],marker='circle',size=20,fill_color='green')
+output_file('scatter.html')
+show(p)
