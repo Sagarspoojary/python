f8f0bf3850fcef375f0036e5cc4510b30add1985
Sagarspoojary <sagar.23cs125@sode-edu.in>
2024-10-24 16:28:21 +0530

    Add files via upload

--- lab8.py
+++ lab8.py
@@ -26,3 +26,31 @@ p=figure(min_width=800,min_height=400)
 p.scatter([1,4,3,2,5],[6,5,2,4,7],marker='circle',size=20,fill_color='green')
 output_file('scatter.html')
 show(p)
+from bokeh.plotting import figure,output_file,show
+p=figure(min_width=800,min_height=400)
+x=[1,2,5,7,9]
+y1=[2,5,4,6,8]
+y2=[5,9,11,12,15]
+p.varea(x=x,y1=y1,y2=y2)
+output_file ('area.html')
+show(p)
+from bokeh.plotting import figure,output_file,show
+from math import pi
+p=figure(min_width=400,min_height=400)
+p.rect(x=[1,2,3],y=[1,2,3],width=0.2,height=40,color='Green',angle=pi/3,height_units='screen')
+output_file ('Rectangle.html')
+show(p)
+import numpy as np
+from bokeh.plotting import figure,output_file,show
+from math import pi
+x=np.linspace(0,4*np.pi,100)
+y=np.sin(x)
+p=figure()
+p.circle(x,y,legend_label="sin(x)")
+p.line(x,y,legend_label="sin(x)")
+p.line(x,2*y,legend_label="2*sin(x)",line_dash=[10,5],line_color='orange',line_width=2)
+p.square(x,3*y,legend_label="3*sin(x)",fill_color=None,line_color='Green')
+p.line(x,3*y,legend_label="3*sin(x)",line_color='green')
+p.legend.location='bottom_left'
+output_file ('Rectangle.html')
+show(p)
